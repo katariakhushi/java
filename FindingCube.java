d34252575b6b9d6d8a9872e60acd4fd4e11304de
katariakhushi <97390769+katariakhushi@users.noreply.github.com>
2023-07-04 22:58:51 +0530

    Add files via upload

---
+++ FindingCube.java
@@ -0,0 +1,10 @@
+public class FindingCube {
+   public static void main(String args[]){
+      
+      System.out.print("Enter a number: ");
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+      System.out.println("Cube of the given number is: "+(num*num*num));
+   }
+}
+
