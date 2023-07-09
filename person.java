b6288e7a563d41b33e02ad0f88b78e47ed1b41fd
katariakhushi <97390769+katariakhushi@users.noreply.github.com>
2023-07-09 23:23:51 +0530

    Add files via upload

---
+++ person.java
@@ -0,0 +1,15 @@
+public class person {
+  String name;
+  int age;
+  float sal;
+    public static void main(String args[]){
+      person p1 = new person();
+      p1.name = "Khushi Kataria";
+      p1.age = 20;
+      p1.sal = 2000000;
+      System.out.println("Name of the person is : " + p1.name);
+      System.out.println("Age of the person is: " + p1.age);
+      System.out.println("Salary of the person is: " + p1.sal);
+
+  }
+}
